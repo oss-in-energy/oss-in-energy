6f0df2bdff8882052cda0570936858c7d06957d1
Jonathan Klimt <jonathan.klimt@eonerc.rwth-aachen.de>
2021-07-21 14:00:05 +0200

    moved git tag search and sort to project-types

--- parser/project_types.py
+++ parser/project_types.py
@@ -1,7 +1,18 @@
+import re
 from dataclasses import dataclass
 from datetime import date
 from typing import Optional
 
+
+def sort_tags_alphanumeric( l ):
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key.name)]
+    return sorted(l, key = alphanum_key)
+
+reg = re.compile("v? ?\d+.\d+(?:.\d+)?") # simple regex to match version strings like "v1.3.4 or v 4.0 or 1.2.3"
+
+def is_release_tag(tag):
+    return reg.match(tag.name)
 
 @dataclass
 class Activity:
